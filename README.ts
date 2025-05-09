884e97609a19cb20fe162c28f6761a519f78d430
timrajan <45014540+timrajan@users.noreply.github.com>
2025-05-09 11:46:30 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,31 +1,14 @@
-/**
- * Gets all XML tag names as a string array
- * @param xmlString The XML string to parse
- * @returns A string array containing all tag names in the XML
- */
-function getAllXmlTagNames(xmlString: string): string[] {
-  // Parse the XML string into a DOM Document
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
-  
-  // Array to store tag names
-  const tagNames: string[] = [];
-  
-  // Recursive function to traverse the XML tree
-  function traverseNodes(node: Node) {
-    // If this is an element node, add its tag name to our array
-    if (node.nodeType === Node.ELEMENT_NODE) {
-      tagNames.push(node.nodeName);
+var itemParams = workitem.Fields["Microsoft.VSTS.TCM.Parameters"];
+    var itemParamsElement = XElement.Parse((string)itemParams);
+
+    var paramDataSource = workitem.Fields["Microsoft.VSTS.TCM.LocalDataSource"];
+    var xElement = XElement.Parse(paramDataSource.ToString());
+
+    //Assuming we have a table named "Table1" in the workitem
+    descendants = xElement.Descendants("Table1");
+
+    foreach (var xe in itemParamsElement.Descendants("param"))
+    {
+      var name = xe.Attribute("name").Value;
+      dt.Columns.Add(name, typeof(string));
     }
-    
-    // Process child nodes
-    for (let i = 0; node.childNodes && i < node.childNodes.length; i++) {
-      traverseNodes(node.childNodes[i]);
-    }
-  }
-  
-  // Start traversal from the document element
-  traverseNodes(xmlDoc.documentElement);
-  
-  return tagNames;
-}
