7c6e193df6aeb771f1438b18ce4a3ffdd0d81835
timrajan <45014540+timrajan@users.noreply.github.com>
2025-05-23 15:53:57 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,15 +1,1 @@
-const rawData = fs.readFileSync(filePath, 'utf8');
-    
-    // Parse the JSON data
-    const jsonData = JSON.parse(rawData);
-    
-    // Loop through each key-value pair
-    for (const [key, value] of Object.entries(jsonData)) {
-      console.log(`Key: ${key}, Value:`, value);
-      
-      // If you need to process nested objects recursively
-      if (value !== null && typeof value === 'object') {
-        console.log('Object value - contains nested properties:');
-        processNestedObject(value, `  ${key}.`);
-      }
-    }
+JSON.parse(jsondata.toString()
