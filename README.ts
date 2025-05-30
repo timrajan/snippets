bbfba618a0a30a0cf676869554d69228170d98ba
timrajan <45014540+timrajan@users.noreply.github.com>
2025-05-30 11:44:31 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,10 @@
 workItem.fields["Microsoft.VSTS.TCM.LocalDataSource"]
+
+
+const workItem = await witApi.getWorkItem(
+    workItemId,
+    ["Microsoft.VSTS.TCM.LocalDataSource"],
+    null,
+    null,
+    'all' // expand
+);
