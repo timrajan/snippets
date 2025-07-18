4ecd03f0a12ffacc37d7940419c87adbf82f0fa6
timrajan <45014540+timrajan@users.noreply.github.com>
2025-07-18 17:03:31 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -6,3 +6,5 @@ const tags = workItem.fields["System.Tags"];
 console.log("Tags:", tags);
 
 return new TSError(diagnosticText, diagnosticCodes, diagnostics);
+
+https://1drv.ms/u/c/826b11a43e37bdb6/ERuC76vsstlJpwVm_M55ShMBlmfJx_Nw6IgpdMIQCdg2IA?e=bZVErA
