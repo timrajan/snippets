d05e7a516f26d02ecd15f4cc9ec950c002d9a34b
timrajan <45014540+timrajan@users.noreply.github.com>
2025-08-14 14:54:04 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -7,3 +7,33 @@ node -e "console.log('Heap limit:', Math.round(require('v8').getHeapStatistics()
 
 set NODE_OPTIONS=--max-old-space-size=4096
 npm test
+
+describe('My tests', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    
+    // Clear all cache types
+    const client = await page.target().createCDPSession();
+    await client.send('Network.clearBrowserCache');
+    await client.send('Network.clearBrowserCookies');
+    await client.send('Storage.clearDataForOrigin', {
+      origin: '*',
+      storageTypes: 'all'
+    });
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
