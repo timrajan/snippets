14f67b85efe3e1e9b533577ebdcd3fde77d4c4c8
timrajan <45014540+timrajan@users.noreply.github.com>
2025-08-14 13:17:10 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -3,3 +3,7 @@ echo %NODE_OPTIONS%
 
 # Check memory limit
 node -e "console.log('Heap limit:', Math.round(require('v8').getHeapStatistics().heap_size_limit / 1024 / 1024), 'MB')"
+
+
+set NODE_OPTIONS=--max-old-space-size=4096
+npm test
