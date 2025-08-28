4b1e7a50abca06d82d9b888352f30603520a678f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-08-28 11:08:35 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,26 +1,24 @@
 {
-  "name": "sharepoint-excel-reader",
-  "version": "1.0.0",
-  "description": "Read Excel data from SharePoint using TypeScript",
-  "main": "dist/index.js",
-  "scripts": {
-    "build": "tsc",
-    "start": "node dist/index.js",
-    "dev": "ts-node src/index.ts",
-    "clean": "rimraf dist"
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "commonjs",
+    "lib": ["ES2020"],
+    "outDir": "./dist",
+    "rootDir": "./src",
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true,
+    "resolveJsonModule": true,
+    "declaration": true,
+    "declarationMap": true,
+    "sourceMap": true
   },
-  "keywords": ["sharepoint", "excel", "typescript", "nodejs"],
-  "author": "Your Name",
-  "license": "MIT",
-  "dependencies": {
-    "@azure/msal-node": "^2.6.6",
-    "axios": "^1.6.7",
-    "xlsx": "^0.18.5"
-  },
-  "devDependencies": {
-    "@types/node": "^20.11.19",
-    "typescript": "^5.3.3",
-    "ts-node": "^10.9.2",
-    "rimraf": "^5.0.5"
-  }
+  "include": [
+    "src/**/*"
+  ],
+  "exclude": [
+    "node_modules",
+    "dist"
+  ]
 }
