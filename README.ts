8a6d3a02748b257b6874e40ba8af48b8cfce2f42
timrajan <45014540+timrajan@users.noreply.github.com>
2025-09-11 14:55:04 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,16 +1,17 @@
-CREATE OR REPLACE FUNCTION calculate_row_position()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION EXCEL_ROW(table_name TEXT)
+RETURNS INTEGER AS $$
 DECLARE
     current_row_num INTEGER;
+    result INTEGER := 0;
+    sql_query TEXT;
 BEGIN
     -- Get the row number of the current row being inserted/updated
-    SELECT COUNT(*) + 1 INTO current_row_num
-    FROM your_table 
-    WHERE id < NEW.id;  -- Assuming id determines the order
-    
+    sql_query := format('SELECT COUNT(*) + 1 INTO current_row_num FROM % WHERE id < NEW.id;',table_name);
+    RAISE NOTICE 'Query: %', sql_query;
+    EXECUTE sql_query INTO result;
     -- Use current_row_num in your formula
-    NEW.formula_column := current_row_num * NEW.some_value;  -- Example formula
+    -- NEW.formula_column := current_row_num * NEW.some_value;  -- Example formula
     
-    RETURN NEW;
+    RETURN result;
 END;
 $$ LANGUAGE plpgsql;
