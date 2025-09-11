20335e9bcdad32cfc602712117cacc03c6fcfedd
timrajan <45014540+timrajan@users.noreply.github.com>
2025-09-11 14:42:17 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,16 @@
--- Simple version - returns position in result set
-CREATE OR REPLACE FUNCTION EXCEL_ROW()
-RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION calculate_row_position()
+RETURNS TRIGGER AS $$
+DECLARE
+    current_row_num INTEGER;
 BEGIN
-    -- This would need to be used within a window function context
-    -- PostgreSQL doesn't have a direct equivalent
-    RETURN NULL;
+    -- Get the row number of the current row being inserted/updated
+    SELECT COUNT(*) + 1 INTO current_row_num
+    FROM your_table 
+    WHERE id < NEW.id;  -- Assuming id determines the order
+    
+    -- Use current_row_num in your formula
+    NEW.formula_column := current_row_num * NEW.some_value;  -- Example formula
+    
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
