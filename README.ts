01b1e67d9dce355545a4ce943e187d384a8c08b4
timrajan <45014540+timrajan@users.noreply.github.com>
2025-09-23 15:26:40 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,8 @@
  TemplateParameters = parameters.ToDictionary(p => p.Key, p => (object)p.Value)  // AND this
+
+
+var templateParameters = new Dictionary<string, object>();
+        foreach (var param in parameters)
+        {
+            templateParameters.Add(param.Key, param.Value);
+        }
