4e670c99a4764d3ff891a94c6ae30b2384a795fc
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-24 13:05:17 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -6,3 +6,11 @@ pg_ctl stop -D "C:\Program Files\PostgreSQL\14\data"
 
 # Force stop
 pg_ctl stop -D "C:\Program Files\PostgreSQL\14\data" -m immediate
+
+
+
+ developed a test data generation and maintenance web application that streamlines QA workflows, reducing the time and effort required for test data preparation."
+
+ empowers our QA team to generate and maintain test data independently, reducing bottlenecks and improving overall testing productivity."
+
+   "reducing test data setup time by X%" or "enabling QA to prepare test environments X times faster."
