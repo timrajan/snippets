22477270d1a97ac9cfa57cb92f1517de54c3fd01
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-28 10:08:29 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,8 +1,3 @@
- flex: 1;
-            width: 100%;
-
-
-
-  min-height: 100vh;
-            display: flex;
-            flex-direction: column;
+ <div class="user-info">
+                    You are logged in as <strong>@ViewBag.Username</strong>
+                </div>
