ef940aa1e669f3ddf626db83be1056c75c2baf22
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-28 11:13:15 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,16 +1,1 @@
- footer {
-            background-color: #2E2B5F;
-            color: white;
-            padding: 20px 0;
-            margin-top: auto;
-            font-size: 14px;
-        }
-
-        footer .container-fluid {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 0 40px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
+<div class="container-fluid">
