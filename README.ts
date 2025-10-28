efefa25346b74e82d361b3c46181cc8dec9cdbe1
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-29 00:34:43 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,29 +1,6 @@
-   .teams-table th {
-        border-right: 1px solid #ddd;
-    }
-
-    .teams-table td:last-child,
-    .teams-table th:last-child {
-        border-right: none;
-    }
+<th class="actions-cell">Actions</th>
 
 
-
-
-
-    .actions-cell {
-        text-align: center;
-        width: 120px;
-    }
-
-    .add-admin-link {
-        color: #4054B5;
-        text-decoration: none;
-        font-size: 14px;
-        font-weight: 500;
-    }
-
-    .add-admin-link:hover {
-        color: #2f3f8f;
-        text-decoration: underline;
-    }
+  <td class="actions-cell">
+                                <a href="/Team/AddAdmin/@team.Id" class="add-admin-link">Add Admin</a>
+                            </td>
