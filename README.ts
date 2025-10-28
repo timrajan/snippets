0da372bfe8cb319c6e76e11f8b757f1638e22a26
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-28 11:26:31 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,14 @@
-<div class="container-fluid">
+    .record-card.disabled {
+        background-color: #cccccc;
+        cursor: not-allowed;
+        pointer-events: none;
+    }
+
+    .record-card.disabled:hover {
+        transform: none;
+        background-color: #cccccc;
+        box-shadow: none;
+    }
+
+
+<a href="/SportsRecord/Index" class="record-card disabled">
