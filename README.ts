c43826f6e3e71a6f6e3717bfc8f14eba02e81ee3
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-28 12:15:19 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,14 +1,9 @@
-    .record-card.disabled {
-        background-color: #cccccc;
-        cursor: not-allowed;
-        pointer-events: none;
-    }
-
-    .record-card.disabled:hover {
-        transform: none;
-        background-color: #cccccc;
-        box-shadow: none;
-    }
-
-
-<a href="/SportsRecord/Index" class="record-card disabled">
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Design" Version="9.0.10">
+      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+      <PrivateAssets>all</PrivateAssets>
+    </PackageReference>
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="9.0.10">
+      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+      <PrivateAssets>all</PrivateAssets>
+    </PackageReference>
+    <PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="9.0.4" />
