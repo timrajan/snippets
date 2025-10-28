f15890111959aadfd65995f07fe1dd6737502f03
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-28 11:12:32 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,16 @@
- .user-info {
+ footer {
+            background-color: #2E2B5F;
             color: white;
+            padding: 20px 0;
+            margin-top: auto;
             font-size: 14px;
         }
 
-
-      display: flex;
+        footer .container-fluid {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 0 40px;
+            display: flex;
             justify-content: space-between;
             align-items: center;
+        }
