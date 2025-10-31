be23f853fe59f26e2c94caec366f358b3ca13bec
timrajan <45014540+timrajan@users.noreply.github.com>
2025-10-31 14:04:23 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,1 @@
- // Enable SQL logging to console
-    options.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
-    options.EnableSensitiveDataLogging(); // Shows parameter values in logs
-    options.EnableDetailedErrors();
+<form method="post" asp-controller="StudyRecord" asp-action="Edit" asp-route-id="@Model.Id">
