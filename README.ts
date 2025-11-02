fad1801a5c976aafcf88968a1830c57552e1ed1f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-03 10:16:01 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,6 +1,13 @@
-width: 100vw;
-            position: relative;
-            left: 50%;
-            right: 50%;
-            margin-left: -50vw;
-            margin-right: -50vw;
+html, body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            min-height: 100vh;
+            overflow-x: hidden;
+            width: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+        }
