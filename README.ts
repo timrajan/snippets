06cead617f05ea2ad04f1b32f2a8cf8ff957da86
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-05 13:55:15 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,5 @@
-InvalidCastException: Reading as 'System.String' is not supported for fields having DataTypeName 'timestamp without time zone'
+curl -u :{YourPAT} \
+  -H "Content-Type: application/json" \
+  -X POST \
+  "https://dev.azure.com/{organization}/{project}/_apis/wit/wiql?api-version=7.0" \
+  -d '{"query": "SELECT [System.Id] FROM WorkItems"}'
