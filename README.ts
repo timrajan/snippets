f27a3dad112b7fc28736b1aab377007c4fadf0fa
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-11 06:01:31 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,22 +1,6 @@
-  ViewBag.Teams = _context.Teams.ToList();
-        ViewBag.SelectedTeamId = teamId;
-
-        // Get team members based on selection
-        List<TeamMember> teamMembers;
-        
-        if (teamId.HasValue)
-        {
-            teamMembers = _context.TeamMembers
-                .Where(m => m.TeamId == teamId.Value)
-                .ToList();
-        }
-        else
-        {
-            teamMembers = _context.TeamMembers.ToList();
-        }
-        
-        ViewBag.TeamMembers = teamMembers;
-
-        // Return the teams list as model (if you need it)
-        var teams = _context.Teams.ToList();
-        return View(teams);
+public class TeamIndexViewModel
+{
+    public List<Team> Teams { get; set; }
+    public List<TeamMember> TeamMembers { get; set; }
+    public int? SelectedTeamId { get; set; }
+}
