561e226b0250592d2c8b488bf3694e97ce2858ec
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-11 06:05:48 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,18 +1,2 @@
-var model = new TeamIndexViewModel
-        {
-            Teams = _context.Teams.ToList(),
-            SelectedTeamId = teamId
-        };
-
-        if (teamId.HasValue)
-        {
-            model.TeamMembers = _context.TeamMembers
-                .Where(m => m.TeamId == teamId.Value)
-                .ToList();
-        }
-        else
-        {
-            model.TeamMembers = _context.TeamMembers.ToList();
-        }
-
-        return View(model);
+@using (Html.BeginForm("Index", "Team", FormMethod.Get))
+{
