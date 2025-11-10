b270d90472688ca5f400527d7db97b482c0a5bc2
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-11 09:49:29 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,8 @@
+The LINQ expression 'DbSet<Member>()
+    .Where(t => t.Id.Equals(
+        value: __username_0, 
+        comparisonType: OrdinalIgnoreCase))' could not be translated. Additional information: Translation of the 'string.Equals' overload with a 'StringComparison' parameter is not supported. See https://go.microsoft.com/fwlink/?linkid=2129535 for more information. Either rewrite the query in a form that can be translated, or switch to client evaluation explicitly by inserting a call to 'AsEnumerable', 'AsAsyncEnumerable', 'ToList', or 'ToListAsync'. See https://go.microsoft.com/fwlink/?linkid=2101038 for more information.
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using StudentManagement.Data;
