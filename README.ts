d68bdb50b9d300c97890ecdb46e60d04500c9a7d
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-10 23:45:03 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,19 +1,8 @@
-  @if (role == "SuperAdmin")
-    {
-        <div class="form-row">
-            <label for="TeamId" class="form-label">Team</label>
-            <select id="TeamId"
-                    name="TeamId"
-                    class="form-input"
-                    required>
-                <option value="">-- Select Team --</option>
-                @if (ViewBag.Teams != null)
-                {
-                    @foreach (var team in (List<StudentManagement.Models.Team>)ViewBag.Teams)
-                    {
-                        <option value="@team.Id">@team.Name</option>
-                    }
-                }
-            </select>
-        </div>
-    }
+ // Get user role
+            var role = ViewBag.Role?.ToString() ?? "TeamAdmin";
+
+            // If SuperAdmin, pass all teams to the view
+            if (role == "SuperAdmin")
+            {
+                ViewBag.Teams = _context.Teams.ToList();
+            }
