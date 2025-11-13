80f4a70d689a8f17b4355e62ba2ea92a7f8df776
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-13 16:24:54 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,28 +1,6 @@
-public class BaseController : Controller
-{
-    // Property to access username in any derived controller
-    public string CurrentUsername
+   [HttpGet("status/mywebhook")]   // For browser
+    [HttpPost("status/mywebhook")]  // For webhook
+    public IActionResult MyWebhook()
     {
-        get
-        {
-            var fullName = User.Identity?.Name;
-            if (string.IsNullOrEmpty(fullName))
-                return "Guest";
-            
-            // Remove domain prefix (DOMAIN\username -> username)
-            var username = fullName.Split('\\').Last();
-            return username;
-        }
+        return Ok(new { status = "Webhook endpoint is active" });
     }
-
-    // Or use this in OnActionExecuting to make it available in all views
-    protected override void OnActionExecuting(ActionExecutingContext context)
-    {
-        base.OnActionExecuting(context);
-        
-        var username = User.Identity?.Name?.Split('\\').Last() ?? "Guest";
-        ViewBag.Username = username;
-        // Or use ViewData
-        // ViewData["Username"] = username;
-    }
-}
