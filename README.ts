4b3cbb438a30387ebe04a5edfe5b7cb43ffc509e
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-13 12:35:40 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,16 +1,28 @@
-builder.Services.AddHttpContextAccessor();
-
-
-private readonly IHttpContextAccessor _httpContextAccessor;
-
-public YourService(IHttpContextAccessor httpContextAccessor)
+public class BaseController : Controller
 {
-    _httpContextAccessor = httpContextAccessor;
-}
+    // Property to access username in any derived controller
+    public string CurrentUsername
+    {
+        get
+        {
+            var fullName = User.Identity?.Name;
+            if (string.IsNullOrEmpty(fullName))
+                return "Guest";
+            
+            // Remove domain prefix (DOMAIN\username -> username)
+            var username = fullName.Split('\\').Last();
+            return username;
+        }
+    }
 
-public string GetUsername()
-{
-    var fullName = _httpContextAccessor.HttpContext?.User?.Identity?.Name;
-    var username = fullName?.Split('\\').Last() ?? "Guest";
-    return username;
+    // Or use this in OnActionExecuting to make it available in all views
+    protected override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+        
+        var username = User.Identity?.Name?.Split('\\').Last() ?? "Guest";
+        ViewBag.Username = username;
+        // Or use ViewData
+        // ViewData["Username"] = username;
+    }
 }
