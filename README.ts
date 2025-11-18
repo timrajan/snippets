4c0f5c229154493bae8d49d52d94ff50e29b62f3
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-18 13:12:50 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,11 +1,20 @@
-// Debug: Log form submission
-    document.querySelector('form').addEventListener('submit', function(e) {
-        console.log('Form submitting to:', this.action);
-        console.log('Method:', this.method);
+ document.addEventListener('DOMContentLoaded', function() {
+        var form = document.querySelector('form');
         
-        var formData = new FormData(this);
-        console.log('Form data:');
-        for (var pair of formData.entries()) {
-            console.log(pair[0] + ': ' + pair[1]);
+        if (form) {
+            console.log('Form found!');
+            
+            form.addEventListener('submit', function(e) {
+                console.log('Form submitting to:', this.action);
+                console.log('Method:', this.method);
+                
+                var formData = new FormData(this);
+                console.log('Form data:');
+                for (var pair of formData.entries()) {
+                    console.log(pair[0] + ': ' + pair[1]);
+                }
+            });
+        } else {
+            console.log('ERROR: Form not found!');
         }
     });
