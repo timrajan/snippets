77a283b72a6c56195a56c61d4a2ec054a2c8792c
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-18 15:33:28 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,10 +1,10 @@
- Console.WriteLine($"===== REQUEST START =====");
-    Console.WriteLine($"Method: {context.Request.Method}");
-    Console.WriteLine($"Scheme: {context.Request.Scheme}"); // http or https
-    Console.WriteLine($"Host: {context.Request.Host}"); // localhost:5000
-    Console.WriteLine($"Path: {context.Request.Path}"); // /Student/Register
-    Console.WriteLine($"PathBase: {context.Request.PathBase}"); // base path if app is in subdirectory
-    Console.WriteLine($"QueryString: {context.Request.QueryString}"); // ?id=123
-    Console.WriteLine($"Full URL: {context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}");
-    Console.WriteLine($"Content-Type: {context.Request.ContentType}");
-    Console.WriteLine($"Has Form: {context.Request.HasFormContentType}");
+dbug: Microsoft.AspNetCore.Mvc.ModelBinding.ParameterBinder[22]
+      Attempting to bind parameter 'record' of type 'Models.Client' ...
+dbug: Microsoft.AspNetCore.Mvc.ModelBinding.Binders.BodyModelBinder[44]
+      Attempting to bind parameter 'record' of type 'Models.Client' using the name '' in request data ...
+dbug: Microsoft.AspNetCore.Mvc.ModelBinding.Binders.BodyModelBinder[2]
+      Rejected input formatter 'Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonInputFormatter' for content type 'application/x-www-form-urlencoded'.
+dbug: Microsoft.AspNetCore.Mvc.ModelBinding.Binders.BodyModelBinder[3]
+      No input formatter was found to support the content type 'application/x-www-form-urlencoded' for use with the [FromBody] attribute.
+dbug: Microsoft.AspNetCore.Mvc.ModelBinding.Binders.BodyModelBinder[4]
+      To use model binding, remove the [FromBody] attribute from the property or parameter named '' with model type 'Models.Client'.
