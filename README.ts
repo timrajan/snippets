09b54d59e1cecbd7f16b867a75468e9f0c560f5b
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-20 14:04:00 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,5 @@
-# Allow connections from any IP address (for testing)
-host    all             all             0.0.0.0/0               md5
-
-# Or for IPv6
-host    all             all             ::/0                    md5
-
-# Allow local connections
-host    all             all             127.0.0.1/32            md5
-host    all             all             ::1/128                 md5
+CREATE TABLE my_table (
+    id SERIAL PRIMARY KEY,
+    eight_digit_number INTEGER NOT NULL,
+    CONSTRAINT check_eight_digits CHECK (eight_digit_number >= 10000000 AND eight_digit_number <= 99999999)
+);
