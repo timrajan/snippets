5df2951e3bf5147596f57b13ce2b329bc3f1a44c
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-21 13:00:49 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -2,18 +2,20 @@
     ViewData["Title"] = "Home Page";
 }
 
-<div class="container my-5">
-    <div class="row">
+<div class="container my-4">
+    <!-- Welcome Heading Row -->
+    <div class="row mb-3">
         <div class="col-12">
-            <h1 class="mb-4">
-                <span class="border-bottom border-dark pb-2">Welcome to the Team Amigos !!!</span>
+            <h1 class="mb-0">
+                <span class="border-bottom border-dark border-3 pb-2">Welcome to the Team Amigos !!!</span>
             </h1>
         </div>
     </div>
     
+    <!-- Content Box Row -->
     <div class="row">
         <div class="col-12">
-            <div class="border border-dark rounded p-4">
+            <div class="border border-dark border-3 rounded p-4">
                 <p class="mb-3">
                     Transform your study space with the Como Desk With Hutch and Two Drawers. Designed 
                     for the hardworking student and the professional on the go, this desk combo brings 
