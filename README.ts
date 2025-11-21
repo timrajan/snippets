0459bac85434b44454f201c991ab7eabc80900ee
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-21 12:46:08 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,28 @@
-{"type":"https://tools.ietf.org/html/rfc9110#section-15.5.1","title":"One or more validation errors occurred.",
+@{
+    ViewData["Title"] = "Home Page";
+}
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="border-bottom border-dark pb-2 mb-4">Welcome to the Team Amigos !!!</h1>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-12">
+            <div class="border border-dark p-4">
+                <p class="mb-0">
+                    Transform your study space with the Como Desk With Hutch and Two Drawers. Designed 
+                    for the hardworking student and the professional on the go, this desk combo brings 
+                    together ample storage and neat organisation for an efficient working experience. Keep 
+                    your books and study materials within easy reach, and enjoy a clutter-free workspace 
+                    that's perfect for both late-night cram sessions and day-long study marathons. With its 
+                    easy-care surface, you'll spend less time cleaning and more time focusing on what 
+                    matters - your future. And the modern, handle-free design adds a touch of sophistication 
+                    to your study area.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
