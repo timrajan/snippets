a4a7b95f6e4bbc264b8dbdf8aeab749f6f74b25f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-21 13:27:33 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,38 +1,16 @@
-@{
-    ViewData["Title"] = "Home Page";
-}
+ .navbar {
+        padding-top: 0.25rem;
+        padding-bottom: 0.25rem;
+        min-height: auto;
+    }
 
-<div class="container my-4">
-    <!-- Welcome Heading Row -->
-    <div class="row mb-3">
-        <div class="col-12">
-            <h1 class="mb-0">
-                <span class="border-bottom border-dark border-3 pb-2">Welcome to the Team Amigos !!!</span>
-            </h1>
-        </div>
-    </div>
+ .navbar {
+        padding: 0;
+        margin-bottom: 0.5rem;
+    }
     
-    <!-- Content Box Row -->
-    <div class="row">
-        <div class="col-12">
-            <div class="border border-dark border-3 rounded p-4">
-                <p class="mb-3">
-                    Transform your study space with the Como Desk With Hutch and Two Drawers. Designed 
-                    for the hardworking student and the professional on the go, this desk combo brings 
-                    together ample storage and neat organisation for an efficient working experience. Keep
-                </p>
-                
-                <ul class="list-unstyled mb-3">
-                    <li class="text-primary">&gt; help resolve issues in team 1</li>
-                    <li class="text-primary">&gt; help resolve issues in team 2</li>
-                    <li class="text-primary">&gt; help resolve issues in team 3</li>
-                    <li class="text-primary">&gt; help resolve issues in team 4</li>
-                </ul>
-                
-                <p class="mb-0">
-                    for more information contact <a href="mailto:admin@help.com">admin@help.com</a>
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
+    .navbar-brand {
+        padding-top: 0;
+        padding-bottom: 0;
+        margin-bottom: 0;
+    }
