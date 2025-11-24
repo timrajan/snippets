ffd6b5eba7fc5187b1c8a0ce4a30e92d0a0c948f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-24 12:31:49 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,16 +1,1 @@
- .navbar {
-        padding-top: 0.25rem;
-        padding-bottom: 0.25rem;
-        min-height: auto;
-    }
-
- .navbar {
-        padding: 0;
-        margin-bottom: 0.5rem;
-    }
-    
-    .navbar-brand {
-        padding-top: 0;
-        padding-bottom: 0;
-        margin-bottom: 0;
-    }
+[Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("YOUR_PAT_HERE"))
