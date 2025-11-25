b4ebb4cbe083465de655034735d311a6db24346b
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-26 10:07:03 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,3 @@
-.\config.cmd --unattended --url https://dev.azure.com/yourorganization --auth pat --token YOUR_PAT_HERE --pool "Default" --agent "AgentName" --proxyurl http://correct-proxy:port --proxyusername "DOMAIN\BBB" --proxypassword "password"
-
-
-.\config.cmd --url https://dev.azure.com/yourorganization --auth pat --token YOUR_PAT --pool "YourPoolName" --agent "Agent1" --proxyurl http://correct-proxy:port --proxyusername "DOMAIN\BBB" --proxypassword "password"
+getTestPlanApiInstance:Error: tunneling socket could not be established, statusCode=407
+Something real bad happened, please fix
+Error: getTestPlanApiInstance: AzureDevOps Api connection error - Please check your personal access token.
