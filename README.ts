50f9e2d9bee06650e7ceb99b487b28cd1031250f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-25 11:57:12 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,5 @@
 getTestPlanApiInstance:Error: tunneling socket could not be established, statusCode=407
+
+.\svc.cmd stop
+.\config.cmd --proxyurl http://correct-proxy:port --proxyusername "DOMAIN\BBB" --proxypassword "your-password"
+.\svc.cmd start
