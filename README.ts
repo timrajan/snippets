33adcb6bccc17c08e32b656d93021e7be0806aed
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-25 16:20:21 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,4 @@
 .\config.cmd --unattended --url https://dev.azure.com/yourorganization --auth pat --token YOUR_PAT_HERE --pool "Default" --agent "AgentName" --proxyurl http://correct-proxy:port --proxyusername "DOMAIN\BBB" --proxypassword "password"
+
+
+.\config.cmd --url https://dev.azure.com/yourorganization --auth pat --token YOUR_PAT --pool "YourPoolName" --agent "Agent1" --proxyurl http://correct-proxy:port --proxyusername "DOMAIN\BBB" --proxypassword "password"
