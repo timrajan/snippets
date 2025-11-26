6ed7696f2e561d8df00962182c0d0e5ed53cf0e6
timrajan <45014540+timrajan@users.noreply.github.com>
2025-11-27 09:43:27 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,1 @@
-getTestPlanApiInstance:Error: tunneling socket could not be established, statusCode=407
-Something real bad happened, please fix
-Error: getTestPlanApiInstance: AzureDevOps Api connection error - Please check your personal access token.
+https://ibm.box.com/s/xyoxq57c7fmhr6ecnj1o247g5hqre2jn
