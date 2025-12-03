9e888409e1cb173885aac29e40b58cd45373847f
timrajan <45014540+timrajan@users.noreply.github.com>
2025-12-03 13:58:00 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,1 @@
-<div class="d-flex form-row col-md-12  flex-fill" style="min-width: 100px; overflow: auto;">
+<input type="text" class="form-control w-100" id="Team" name="Team" value="dfhggdfgjhgfjhgdjfgdjfja" readonly />
