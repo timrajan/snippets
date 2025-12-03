2f4cfd39eff9effd9675824b6afdeeaa336dfbda
timrajan <45014540+timrajan@users.noreply.github.com>
2025-12-03 13:29:58 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,21 +1,25 @@
-  .results-table {
-      /* width: 80%;
-      max-width: 1200px;
-      border-collapse: collapse;
-      margin: 20px auto 0 auto;
-      text-align: left; */
-  }
-
-
-      .results-table th {
-          /* background-color: #f5f5f5;
-          text-align: left;
-          padding: 15px;
-          font-weight: bold;
-          font-size: 16px;
-          color: #000;
-          border: 1px solid #ddd;
-          cursor: pointer;
-          user-select: none;
-          position: relative; */
-      }
+<form>
+  <div class="row g-3 mb-3">
+    <div class="col-md-4 d-flex align-items-center gap-2">
+      <label>first</label>
+      <input type="text" class="form-control">
+    </div>
+    <div class="col-md-4 d-flex align-items-center gap-2">
+      <label>middle</label>
+      <input type="text" class="form-control">
+    </div>
+    <div class="col-md-4 d-flex align-items-center gap-2">
+      <label>last</label>
+      <input type="text" class="form-control">
+    </div>
+  </div>
+  
+  <div class="mb-3">
+    <label class="form-label">update comments</label>
+    <textarea class="form-control" rows="3"></textarea>
+  </div>
+  
+  <div class="text-center">
+    <button type="submit" class="btn btn-outline-dark">Update</button>
+  </div>
+</form>
