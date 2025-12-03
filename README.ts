de9171daafb350f99976ee6a44e517a1f2b6e725
timrajan <45014540+timrajan@users.noreply.github.com>
2025-12-03 13:51:20 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,14 +1,1 @@
-<div class="d-flex flex-nowrap gap-3 mb-3">
-  <div class="d-flex align-items-center gap-2 flex-fill">
-    <label>first</label>
-    <input type="text" class="form-control">
-  </div>
-  <div class="d-flex align-items-center gap-2 flex-fill">
-    <label>middle</label>
-    <input type="text" class="form-control">
-  </div>
-  <div class="d-flex align-items-center gap-2 flex-fill">
-    <label>last</label>
-    <input type="text" class="form-control">
-  </div>
-</div>
+<input type="text" class="form-control" style="min-width: 100px; overflow: auto;">
