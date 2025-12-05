0c5e18a0e51e72e8dbf02eeb1ed4cedb568d1f7d
timrajan <45014540+timrajan@users.noreply.github.com>
2025-12-05 12:21:13 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    var tooltipList = [...tooltipTriggerList].map(function(tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
-
-
-
-<span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip" data-bs-title="This link is currently disabled">
-    <a href="#" class="disabled" aria-disabled="true" tabindex="-1" style="pointer-events: none;">
-        Disabled Link
-    </a>
-</span>
+Queries performing 'LastOrDefault' operation must have a deterministic sort order. Rewrite the query to apply an 'OrderBy' operation on the sequence before calling 'LastOrDefault'.
