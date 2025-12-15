4239c94f18aa5f0ee0495fff98073a4041692b08
timrajan <45014540+timrajan@users.noreply.github.com>
2025-12-15 14:50:13 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,13 +1,1 @@
-41 verbose stack HttpErrorAuthUnknown: Unable to authenticate, need: Basic realm="Artifactory Realm"
-npm error code E401
-npm error Incorrect or missing password.
-npm error If you were trying to login, change your password, create an
-npm error authentication token or enable two-factor authentication then
-npm error that means you likely typed your password in incorrectly.
-npm error Please try again, or recover your password at:
-npm error   https://www.npmjs.com/forgot
-npm error
-npm error If you were doing some other operation then your saved credentials are
-npm error probably out of date. To correct this please try logging in again with:
-npm error   npm login
- 
+[Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("your-password"))
