ff80fa86b35774f6ce981999bacc36f35a24b658
timrajan <45014540+timrajan@users.noreply.github.com>
2026-02-20 11:49:21 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,24 +1,17 @@
-CREATE OR REPLACE FUNCTION ROW_FUNC(table_name TEXT)   --TODO
-RETURNS INTEGER AS $$
-DECLARE
-    next_row_num INTEGER;
-    sql_query TEXT;
-BEGIN
-    sql_query := format('SELECT COUNT(*) - 1 FROM %I', table_name);
-    EXECUTE sql_query INTO next_row_num;
-    RETURN next_row_num;
-END;
-$$ LANGUAGE plpgsql;
+public int RowFunc(string tableName)
+{
+    var count = _context.Database
+        .SqlQueryRaw<int>($"SELECT COUNT(*) FROM \"{tableName}\"")
+        .AsEnumerable()
+        .First();
+    return count - 1;
+}
 
-
-CREATE OR REPLACE FUNCTION PREV_ROW_FUNC(table_name TEXT)  --TODO
-RETURNS INTEGER AS $$
-DECLARE
-    next_row_num INTEGER;
-    sql_query TEXT;
-BEGIN
-    sql_query := format('SELECT COUNT(*) - 2 FROM %I', table_name);
-    EXECUTE sql_query INTO next_row_num;
-    RETURN next_row_num;
-END;
-$$ LANGUAGE plpgsql;
+public int PrevRowFunc(string tableName)
+{
+    var count = _context.Database
+        .SqlQueryRaw<int>($"SELECT COUNT(*) FROM \"{tableName}\"")
+        .AsEnumerable()
+        .First();
+    return count - 2;
+}
