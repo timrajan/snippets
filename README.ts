22ca74da4b73fd495ba0c59d728cc0e7a70ab302
timrajan <45014540+timrajan@users.noreply.github.com>
2026-02-20 11:48:00 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,20 +1,24 @@
-public class MyDbContext : DbContext
-{
-    private readonly string _connectionString;
-
-    public MyDbContext(string connectionString)
-    {
-        _connectionString = connectionString;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql(_connectionString);
-    }
-
-    public DbSet<MyEntity> MyEntities { get; set; }
-}
+CREATE OR REPLACE FUNCTION ROW_FUNC(table_name TEXT)   --TODO
+RETURNS INTEGER AS $$
+DECLARE
+    next_row_num INTEGER;
+    sql_query TEXT;
+BEGIN
+    sql_query := format('SELECT COUNT(*) - 1 FROM %I', table_name);
+    EXECUTE sql_query INTO next_row_num;
+    RETURN next_row_num;
+END;
+$$ LANGUAGE plpgsql;
 
 
-var connectionString = "Host=localhost;Port=5432;Database=mydb;Username=myuser;Password=mypass";
-using var _dbContext = new MyDbContext(connectionString);
+CREATE OR REPLACE FUNCTION PREV_ROW_FUNC(table_name TEXT)  --TODO
+RETURNS INTEGER AS $$
+DECLARE
+    next_row_num INTEGER;
+    sql_query TEXT;
+BEGIN
+    sql_query := format('SELECT COUNT(*) - 2 FROM %I', table_name);
+    EXECUTE sql_query INTO next_row_num;
+    RETURN next_row_num;
+END;
+$$ LANGUAGE plpgsql;
