2c553cecf4a2939e6a6ebd0d7ad4eb19b5267735
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-03 12:21:28 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,2 +1,5 @@
 $ConfirmPreference = "None"
 $PSDefaultParameterValues['*:Confirm'] = $false
+$PSDefaultParameterValues['*:Force'] = $true
+$InformationPreference = "Continue"
+$ProgressPreference = "SilentlyContinue"
