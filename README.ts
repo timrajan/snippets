0dbb9111d41158156ea56f9005ec5756ec06fa40
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-13 16:50:12 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -2,3 +2,11 @@
       var json = JsonSerializer.Serialize(payload);
       var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
       var response = httpClient.PostAsync(url, content).GetAwaiter().GetResult();
+
+
+var json = JsonSerializer.Serialize(payload, new JsonSerializerOptions
+{
+    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+});
+var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+var response = httpClient.PostAsync(url, content).GetAwaiter().GetResult();
