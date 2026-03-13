a02cf9294105774dd4f978a290aaa021775e1c6d
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-13 17:22:28 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,15 +1,18 @@
-  var payload = new
-  {
-      definition = new
-      {
-          id = _pipelineId
-      },
-      sourceBranch = _branch,
-      parameters = (new
-      {
-          // Map form fields  pipeline parameters
-          param1 = param1value,
-          param2 = param2value,
-      }
-      )
-  };
+var url = $"https://dev.azure.com/{_org}/{_project}/_apis/pipelines/{_pipelineId}/runs?api-version=6.0-preview";
+
+
+var payload = new
+{
+    templateParameters = new Dictionary<string, string>
+    {
+        { "param1", param1value },
+        { "param2", param2value }
+    },
+    resources = new
+    {
+        repositories = new
+        {
+            self = new { refName = $"refs/heads/{_branch}" }
+        }
+    }
+};
