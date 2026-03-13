4ac8e73a2be5aa30937b4291e36b2bb517b89035
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-13 17:14:51 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,5 +1,15 @@
-   templateParameters = new Dictionary<string, string>
-    {
-        { "param1", paramValue1 },
-        { "param2", xmlString }  // raw string, not pre-serialized
-    }
+  var payload = new
+  {
+      definition = new
+      {
+          id = _pipelineId
+      },
+      sourceBranch = _branch,
+      parameters = (new
+      {
+          // Map form fields  pipeline parameters
+          param1 = param1value,
+          param2 = param2value,
+      }
+      )
+  };
