55c86d68543947b1077ea33e3618f3daef6e2d4b
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-19 16:44:53 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-var username = User?.Identity?.IsAuthenticated == true
-    ? User.Identity.Name?.Split('\\').Last()
-    : Environment.UserName; // fallback for dev if auth not set up
-@using System.Linq
-
-@{
-    var username = User?.Identity?.IsAuthenticated == true
-        ? User.Identity.Name?.Split('\\').Last()
-        : Environment.UserName;
-}
-
-<!-- Then use it anywhere in the HTML -->
-<p>Welcome, @username</p>
+var response = Task.Run(() => _httpClient.PostAsync(url, content)).GetAwaiter().GetResult();
