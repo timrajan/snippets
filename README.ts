8997566c7daf67a6a23f773b54759be2f11ebbc3
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-24 09:39:59 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,13 +1,1 @@
-// Add this after your HTTP call
-File.AppendAllText(@"C:\temp\devops_debug.txt", 
-    $"{DateTime.Now}: Status={response.StatusCode}, Body={body}{Environment.NewLine}");
-
-// Add this inside your catch block
-File.AppendAllText(@"C:\temp\devops_debug.txt", 
-    $"{DateTime.Now}: ERROR={ex.Message}, Stack={ex.StackTrace}{Environment.NewLine}");
-
-
-mkdir C:\temp
-icacls "C:\temp" /grant "IIS AppPool\YourAppPoolName:(OI)(CI)F"
-
-Get-Content C:\temp\devops_debug.txt
+24/03/2026 9:39:03 AM: ERROR=No such host is known. (dev.azure.com:443), Stack=   at System.Net.Http.HttpConnectionPool.ConnectToTcpHostAsync(String host, Int32 port, HttpRequestMessage initialRequest, Boolean async, CancellationToken cancellationToken)
