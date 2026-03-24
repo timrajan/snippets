5ddd8e681034f8d32c3ef096610123770391fa50
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-25 09:48:42 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,2 +1,5 @@
-builder.Services.AddAuthentication(IISDefaults.AuthenticationScheme).AddCookie();
-builder.Services.AddAuthorization();
+# Get ALL settings of other app pool
+& "$env:windir\system32\inetsrv\appcmd" list apppool "OtherAppPoolName" /config:*
+
+# Get ALL settings of your app pool
+& "$env:windir\system32\inetsrv\appcmd" list apppool "YourAppPoolName" /config:*
