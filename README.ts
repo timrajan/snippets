9da029df644ea6fc37667dffe47d13815fa2ef27
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-25 10:08:44 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,5 +1,1 @@
-# Get ALL settings of other app pool
-& "$env:windir\system32\inetsrv\appcmd" list apppool "OtherAppPoolName" /config:*
-
-# Get ALL settings of your app pool
-& "$env:windir\system32\inetsrv\appcmd" list apppool "YourAppPoolName" /config:*
+Get-Process -Name "w3wp" -IncludeUserName | Select-Object Name, UserName
