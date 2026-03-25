09fdeb9193043d2b69ac80050fc5b36d92c0a166
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-25 15:49:41 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,25 @@
-Get-Process -Name "w3wp" -IncludeUserName | Select-Object Name, UserName
+
+
+
+@if (TempData["Message"] != null)
+{
+    var msgType = TempData["MessageType"] ?? "success";
+
+    <div id="appAlert" 
+     class="alert alert-@msgType alert-dismissible fade show position-fixed top-50 start-50 translate-middle p-4"
+     role="alert"
+     style="z-index: 9999; min-width: 600px;">
+    @TempData["Message"]
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+</div>
+
+    <script>
+        setTimeout(() => {
+            const el = document.getElementById('appAlert');
+            if (el) {
+                el.classList.remove('show');
+                setTimeout(() => el.remove(), 500);
+            }
+        }, 3000);
+    </script>
+}
