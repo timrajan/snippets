db960efc3716fe4860380df516d99d4d921a29c8
timrajan <45014540+timrajan@users.noreply.github.com>
2026-03-31 17:10:07 +1100

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,36 +1,8 @@
-<style>
-    .status-new {
-        color: blue;
-        font-weight: bold;
-    }
-    .status-action {
-        color: green;
-    }
-    .status-failed {
-        color: red;
-        font-weight: bold;
-    }
-</style>
-
-
-    <table>
-    <thead>
-        <tr>
-            <th>Status</th>
-            <!-- your other columns -->
-        </tr>
-    </thead>
-    <tbody>
-        @foreach (var item in ViewBag.Results)
-        {
-            <tr>
-                <td class="@(item.Status == "new" ? "status-new" : 
-                             item.Status == "action" ? "status-action" : 
-                             item.Status == "failed" ? "status-failed" : "")">
-                    @item.Status
-                </td>
-                <!-- your other columns -->
-            </tr>
-        }
-    </tbody>
-</table>
+\<system.webServer>
+  <aspNetCore processPath="dotnet" arguments=".\YourApp.dll">
+    <environmentVariables>
+      <environmentVariable name="HTTPS_PROXY" value="http://proxyname:port" />
+      <environmentVariable name="HTTP_PROXY" value="http://proxyname:port" />
+    </environmentVariables>
+  </aspNetCore>
+</system.webServer>
