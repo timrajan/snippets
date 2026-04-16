c71d055910000c0a22e2cf46db8ae52ac823d80f
timrajan <45014540+timrajan@users.noreply.github.com>
2026-04-16 10:32:02 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,96 @@
-var response = Task.Run(() => _httpClient.PostAsync(url, content)).GetAwaiter().GetResult();
+Iran is home to one of the hottest places on Earth, a UNESCO-listed desert where surface temperatures have reached over 70 degrees Celsius. What is the name of this desert?
+
+A) Gobi Desert
+
+B) Sahara Desert
+
+C) Lut Desert (Dasht-e Lut)
+
+D) Kalahari Desert
+
+
+Hormuz Island in Iran is often nicknamed “Rainbow Island.” What is the main reason for this name?
+
+A) Its brightly painted houses
+
+B) Its multicolored soils, rocks, and mountains 
+
+C) Its year‑round double rainbows
+
+D) Its colorful coral reefs
+
+Which European power controlled Hormuz Island until it was retaken by Anglo‑Persian forces in 1622?
+
+A) Spain
+
+B) Portugal 
+
+C) The Netherlands
+
+D) France
+
+
+Why is the Strait of Hormuz considered one of the world’s most strategic waterways?
+
+A) It is the only entry to the Suez Canal
+
+B) It connects the Persian Gulf to the Gulf of Oman and a large share of global seaborne oil passes through 
+
+C) It is the deepest ocean trench on Earth
+
+D) It is the main route for Arctic sea ice export
+
+In which year did the United States and Saudi Arabia symbolically cement their emerging oil-based relationship with a meeting between President Franklin D. Roosevelt and King Abdulaziz aboard a U.S. ship?
+
+A) 1933
+
+B) 1945 
+
+C) 1956
+
+D) 1967
+
+
+What was the birth name of Pope Leo XIV, the first American‑born pope elected in 2025?
+
+A. James Michael Harvey
+B. Pietro Parolin
+C. Robert Francis Prevost
+D. Raymond Leo Burke
+
+
+Who abdicated the throne of England in the 1930s in order to marry Wallis Simpson?
+
+A. King George V
+B. King Edward VII
+C. King Edward VIII
+D. King George VI
+
+
+Prince Harry, Duke of Sussex, is a member of which royal house of the British royal family?
+
+A. House of Tudor
+B. House of Stuart
+C. House of Windsor
+D. House of Hanover
+
+What is the full given name of Prince Harry, Duke of Sussex?
+
+A. Harry Charles William David
+B. Henry Charles Albert David
+C. Henry Arthur Philip George
+D. Harry James Albert Charles
+
+
+What are the names of Prince Harry and Meghan, Duchess of Sussex’s two children?
+
+A. George and Charlotte
+B. Archie and Lilibet
+C. Louis and Mia
+D. James and Beatrice
+
+
+
+
+
+
