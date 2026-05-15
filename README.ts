b9de9ad79d3014e2618911cf2b471e3ef8c2d497
timrajan <45014540+timrajan@users.noreply.github.com>
2026-05-15 13:10:08 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,2 @@
 const SHARED_STEP_XML = `<steps id="0" last="1"><step id="2" type="SharedStep" ref="${SHARED_STEP_ID}"><description/></step></steps>`;
+const SHARED_STEP_XML = `<steps id="0" last="2"><compref ref="${SHARED_STEP_ID}" id="2"/></steps>`;
