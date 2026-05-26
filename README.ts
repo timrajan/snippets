919557a4c80c5972c49f0681309b61974941e802
timrajan <45014540+timrajan@users.noreply.github.com>
2026-05-26 11:15:08 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -8,6 +8,17 @@ puppeteer.launch({
   ]
 });
 
+const host = 'colleague-machine-name';
+puppeteer.launch({
+  args: [
+    `--proxy-bypass-list=<-loopback>;${host};${host}:19003`,
+    `--auth-server-allowlist=${host}`,
+    `--auth-negotiate-delegate-allowlist=${host}`
+  ]
+});
+
+
+
 page.on('request', req => process.stdout.write(`→ ${req.method()} ${req.url()}\n`));
 page.on('response', res => process.stdout.write(`← ${res.status()} ${res.url()}\n`));
 page.on('requestfailed', req => process.stdout.write(`✗ ${req.url()} ${req.failure()?.errorText}\n`));
