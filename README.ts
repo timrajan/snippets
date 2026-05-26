09a9f6a59225a182a4e808f915cd57376334f960
timrajan <45014540+timrajan@users.noreply.github.com>
2026-05-26 11:14:34 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,6 +1,13 @@
 process.stdout.write('=== TEST FILE LOADED ===\n');
 process.stdout.write('=== PAGE CREATED, ATTACHING LISTENERS ===\n');
 
+puppeteer.launch({
+  args: [
+    `--auth-server-allowlist=${colleagueMachineName}`,
+    `--auth-negotiate-delegate-allowlist=${colleagueMachineName}`
+  ]
+});
+
 page.on('request', req => process.stdout.write(`→ ${req.method()} ${req.url()}\n`));
 page.on('response', res => process.stdout.write(`← ${res.status()} ${res.url()}\n`));
 page.on('requestfailed', req => process.stdout.write(`✗ ${req.url()} ${req.failure()?.errorText}\n`));
