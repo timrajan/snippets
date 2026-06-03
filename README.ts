b190a302af75bbf4c48e592c6458228ac3325672
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-03 10:32:50 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,10 @@
 const browser = await puppeteer.launch({ headless: true });
 const pid = browser.process()?.pid;
 console.log('Browser PID:', pid, '| Node account:', require('os').userInfo().username);
+
+
+- powershell: |
+    whoami
+    nslookup something.azureedge.net
+    curl -v https://something.azureedge.net
+  displayName: 'Prove service account cannot reach azureedge.net'
