890c31f185c0308e502a01ee2f871b7b28ee05cc
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-12 11:31:10 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,9 +1,1 @@
-pg_dump -U your_user -d your_database \
-  -t table1 -t table2 -t table3 -t table4 -t table5 \
-  -F c -f tables.dump
-
-
-pg_restore -U postgres -d your_existing_db /tmp/tables.dump
-
-
-C:\Program Files\PostgreSQL\16\bin\
+The root cause is that Azure DevOps pipeline variables are always strings. When the pipeline passed $(myVariable) into your setup.ps1's [bool] parameter, PowerShell received the literal string "true"/"false" and refused to bind it to a [bool].
