1c71c5560d5167c79ddaccee3feb17accc55b780
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-15 11:17:30 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,15 +1,9 @@
- var httpClient = new HttpClient();
- httpClient.BaseAddress= new Uri(_org);
- httpClient.DefaultRequestHeaders.Authorization =
-     new AuthenticationHeaderValue("Bearer", _pat.Trim());
-
-
-
-var url = $"{Uri.EscapeDataString(_project)}/_apis/build/builds/{runId}?api-version=7.1";
- using var resp = await http.GetAsync(url, ct);
-
- if (!resp.IsSuccessStatusCode)
- {
-     _logger.LogWarning("ADO returned {code} for run {id}", (int)resp.StatusCode, runId);
-     return (false, "", null);
- }
+private HttpClient CreateAdoClient()
+{
+    var http = _httpFactory.CreateClient("ado");
+    http.BaseAddress = new Uri(_org.TrimEnd('/') + "/");
+    http.Timeout = TimeSpan.FromSeconds(20);
+    http.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", _pat.Trim());
+    return http;
+}
