047cf70559d6d00c11906342f807c1e582642977
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-15 11:10:48 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,8 @@
-System.AggregateException: 'Some services are not able to be constructed (Error while validating the service descriptor 'ServiceType: Microsoft.Extensions.Hosting.IHostedService Lifetime: Singleton ImplementationType: Services.BuildPollingService': Unable to resolve service for type 'System.Net.Http.IHttpClientFactory' while attempting to activate 
+ var url = $"{Uri.EscapeDataString(_project)}/_apis/build/builds/{runId}?api-version=7.1";
+ using var resp = await http.GetAsync(url, ct);
+
+ if (!resp.IsSuccessStatusCode)
+ {
+     _logger.LogWarning("ADO returned {code} for run {id}", (int)resp.StatusCode, runId);
+     return (false, "", null);
+ }
