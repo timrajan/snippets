cacf0716a4d0293c13b7e870b76195c5d6c1b58d
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-15 11:14:18 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,11 @@
- var url = $"{Uri.EscapeDataString(_project)}/_apis/build/builds/{runId}?api-version=7.1";
+ var httpClient = new HttpClient();
+ httpClient.BaseAddress= new Uri(_org);
+ httpClient.DefaultRequestHeaders.Authorization =
+     new AuthenticationHeaderValue("Bearer", _pat.Trim());
+
+
+
+var url = $"{Uri.EscapeDataString(_project)}/_apis/build/builds/{runId}?api-version=7.1";
  using var resp = await http.GetAsync(url, ct);
 
  if (!resp.IsSuccessStatusCode)
