e069c42a2251b8111f4a825be5c7112c50b8bb81
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-16 15:43:41 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,19 @@
-HTTP Error 500.31 - Failed to load ASP.NET Core runtime
+private readonly IConfiguration _configuration;
+
+public BuildController(IConfiguration configuration)
+{
+    _configuration = configuration;
+}
+
+In PowerShell: [guid]::NewGuid().ToString("N")
+
+
+
+X-Secret-Token: a7f3c9e2b8d4
+
+
+{
+  "Webhook": {
+    "SecretToken": "a7f3c9e2b
+  }
+}
