76b7a5ba84d82752e9416eaf47b0308e1d384335
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-17 11:23:25 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,18 +1,3 @@
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    
-    app.UseHsts();
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage(); // Shows detailed error pages
-}
-else
-{
-    app.UseDeveloperExceptionPage(); // Shows detailed error pages
-
-}
+appcmd unlock config -section:system.webServer/httpErrors
+appcmd unlock config -section:system.webServer/security/authentication/anonymousAuthentication
+appcmd unlock config -section:system.webServer/security/authentication/windowsAuthentication
