9f8a0db4da593322801cf469345a4f311fbc6825
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-17 10:36:36 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,10 +1,2 @@
-<location path="apath/bpath/webhook">
-  <system.webServer>
-    <security>
-      <authentication>
-        <anonymousAuthentication enabled="true" />
-        <windowsAuthentication enabled="false" />
-      </authentication>
-    </security>
-  </system.webServer>
-</location>
+ <environmentVariable name="ASPNETCORE_ENVIRONMENT" value="Development" />
+</environmentVariable>
