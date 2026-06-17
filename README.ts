9126a6a4e6749e3e7d48930d3deef99af49be84f
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-17 10:48:25 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,1 @@
- <environmentVariable name="ASPNETCORE_ENVIRONMENT" value="Development" />
-  <environmentVariable name="ASPNETCORE_DETAILEDERRORS" value="true" />
-
+<httpErrors existingResponse="PassThrough" />
