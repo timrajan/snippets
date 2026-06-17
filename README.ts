2494aac8b1eb0eb4151a97085f5ef187562d769c
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-17 10:59:06 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,18 @@
-<httpErrors existingResponse="PassThrough" />
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    
+    app.UseHsts();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // Shows detailed error pages
+}
+else
+{
+    app.UseDeveloperExceptionPage(); // Shows detailed error pages
+
+}
