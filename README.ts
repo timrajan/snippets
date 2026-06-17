04ebc85830fbac892b7296a77f3bc7b9b8f718d0
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-17 12:06:24 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,11 @@
 appcmd unlock config -section:system.webServer/httpErrors
 appcmd unlock config -section:system.webServer/security/authentication/anonymousAuthentication
 appcmd unlock config -section:system.webServer/security/authentication/windowsAuthentication
+
+<remove name="WebDAVModule" />
+
+<modules>
+    <remove name="WebDAVModule" />
+</modules>
+
+  unlock with appcmd unlock config -section:system.webServer/modules (and /handlers)
