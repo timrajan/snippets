8f7a3c81a1bdf653a31ecc7ff03ad21975d86cc0
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-18 10:48:12 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,15 +1,7 @@
-<location path="apath/bpath/webhook">
-  <system.webServer>
-    <security>
-      <authentication>
-        <anonymousAuthentication enabled="true" />
-        <windowsAuthentication enabled="false" />
-      </authentication>
-    </security>
-    <handlers>
-      <clear />
-      <add name="aspNetCore" path="*" verb="*"
-           modules="AspNetCoreModuleV2" resourceType="Unspecified" />
-    </handlers>
-  </system.webServer>
-</location>
+Here's a one-liner for the PR:
+
+Register tsconfig-paths in the ts-node run so the @framework/* path aliases resolve outside Jest (ts-node doesn't read tsconfig paths on its own), fixing the MODULE_NOT_FOUND when running run-test-suite.ts.
+
+If you want it even tighter for a commit subject line:
+
+Add tsconfig-paths/register to ts-node so @framework aliases resolve when running suites directly
