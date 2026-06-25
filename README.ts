24003f3d72ee84c7c729bdab09117501f56840dc
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-26 09:15:21 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,18 +1,25 @@
 public async Task<JsonResult> GetNextId()
 {
-    int nextId;
+    int nextId, nextCode;
     var conn = _context.Database.GetDbConnection();
     await conn.OpenAsync();
     try
     {
         using var cmd = conn.CreateCommand();
-        cmd.CommandText = "SELECT nextval(pg_get_serial_sequence('users', 'id'));";
-        nextId = Convert.ToInt32(await cmd.ExecuteScalarAsync());
+        cmd.CommandText = @"
+            SELECT nextval(pg_get_serial_sequence('users', 'id')) AS new_id,
+                   nextval('table_name_code_seq')                AS new_code;";
+
+        using var reader = await cmd.ExecuteReaderAsync();
+        await reader.ReadAsync();
+
+        nextId   = reader.GetInt32(0);
+        nextCode = reader.GetInt32(1);
     }
     finally
     {
         await conn.CloseAsync();
     }
 
-    return Json(new { id = nextId });
+    return Json(new { id = nextId, code = nextCode });
 }
