4afada028bf390a85a8b07b3082089c136e8e4e5
timrajan <45014540+timrajan@users.noreply.github.com>
2026-06-29 15:15:29 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,36 +1,13 @@
-public async Task<JsonResult> GetNextId()
+[HttpPost]
+public IActionResult ReceiveWebhook([FromBody] JsonElement payload)
 {
-    int nextId, nextCode;
-    var conn = _context.Database.GetDbConnection();
-    await conn.OpenAsync();
-    try
-    {
-        using var cmd = conn.CreateCommand();
-        cmd.CommandText = @"
-            SELECT nextval(pg_get_serial_sequence('users', 'id')) AS new_id,
-                   nextval('table_name_code_seq')                AS new_code;";
+    var json = payload.GetRawText();
 
-        using var reader = await cmd.ExecuteReaderAsync();
-        await reader.ReadAsync();
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "webhook_payloads.txt");
 
-        nextId   = reader.GetInt32(0);
-        nextCode = reader.GetInt32(1);
-    }
-    finally
-    {
-        await conn.CloseAsync();
-    }
+    System.IO.File.AppendAllText(
+        filePath,
+        $"{DateTime.UtcNow:O}\t{json}{Environment.NewLine}");
 
-    return Json(new { id = nextId, code = nextCode });
-}
-
-
-function launchForm() {
-    fetch('/User/GetNextId')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('userIdField').value = data.id;
-            document.getElementById('codeField').value = data.code;
-        })
-        .catch(error => console.error('Error:', error));
+    return Ok();
 }
