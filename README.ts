cf84eb22662018e3995c4812fe5076977e93476f
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-06 15:07:04 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -5,7 +5,13 @@ test.ts:10:26 - error TS7016: Could not find a declaration file for module 'xlsx
 10 import XlsxPopulate from "xlsx-populate";
                            
 
-
+{
+  "ts-node": {
+    "files": true
+  },
+  "files": ["types/xlsx-populate.d.ts"],
+  ...
+}
 
 
 
