236c82b5b0f5c217522257d8d997ceb0360725bd
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-07 12:54:03 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,19 +1,5 @@
-rows.forEach(row => {
-  if (row['Date']) row['Date'] = usToAu(row['Date']);
-});
-
-function usToAu(s: string): string {
-  const [m, d, y] = s.split('/');
-  return `${d}/${m}/${y}`;
-}
-
-
-const workbook = XLSX.readFile('file.xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const rows = XLSX.utils.sheet_to_json<any>(sheet, { raw: false, dateNF: 'dd/mm/yyyy' });
-
-// fix the date column immediately, once, for all rows:
-rows.forEach(row => {
-  if (row['Date']) row['Date'] = usToAu(row['Date']);
-});
-
+  const iframeHandle = await page.$("iframe");
+    if (iframeHandle) {
+        const frame = await iframeHandle.contentFrame();
+        if (frame) page = frame as unknown as Page; // <-- reassigning page to the frame
+    }
