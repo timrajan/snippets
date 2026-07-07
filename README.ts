94d4754e113d613863afec969f5d1b2a071d3de6
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-07 10:24:15 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,3 @@
-{
-  "include": ["src/**/*", "types/**/*"]
-}
+           if (global.testError) {
+                           ~~~~~~~~~
+    jest-setup.ts:117:58 - error TS7017: Element implicitly has an 'any' type because type 'typeof globalThis' has no index signature.
