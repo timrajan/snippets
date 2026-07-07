87aa6df139ce88f86fc3d3af5d8e2a667604f266
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-07 10:19:20 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,5 +1,3 @@
-import * as XLSX from 'xlsx';
-
-const workbook = XLSX.readFile('file.xlsx', { cellDates: true });
-// or for buffers:
-const workbook = XLSX.read(buffer, { type: 'buffer', cellDates: true });
+{
+  "include": ["src/**/*", "types/**/*"]
+}
