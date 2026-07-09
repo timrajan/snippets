fde0ba35fe2aae3037349da8fb2c1e43ce13b4c3
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-09 15:17:52 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -7,3 +7,8 @@ WHERE datname = 'abc' AND pid <> pg_backend_pid();
 
 -- Clone it
 CREATE DATABASE xyz TEMPLATE abc;
+
+
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'abc' AND pid <> pg_backend_pid();
