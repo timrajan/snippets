8551aa2df5c548152d9c238994043dbf4d71d1e8
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-10 12:15:54 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,23 +1,5 @@
-{
-  "ConnectionStrings": {
-    "ProductionConnection": "Host=myserver;Port=5432;Database=myapp_prod;Username=appuser;Password=xxx",
-    "StagingConnection": "Host=myserver;Port=5432;Database=myapp_staging;Username=appuser;Password=xxx"
-  }
-}
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ProductionConnection")));
 
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<Customer> Customers { get; set; }
-    // ... all your entities
-}
-
-public class StagingDbContext : AppDbContext
-{
-    public StagingDbContext(DbContextOptions<StagingDbContext> options)
-        : base(options) { }
-}
-
+builder.Services.AddDbContext<StagingDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("StagingConnection")));
