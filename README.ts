76c35c5f7dbf7e60e5b714a88ce47105bf285b2d
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-16 12:50:08 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -3,3 +3,12 @@ const result = {
         witClient: await connection.getWorkItemTrackingApi(),
         testClient: await connection.getTestPlanApi(),
     };
+
+
+interface AdoClients {
+    gitClient: IGitApi;
+    witClient: IWorkItemTrackingApi;
+    testClient: ITestPlanApi;
+}
+
+async function getAdoClients(connection: WebApi): Promise<AdoClients> {
