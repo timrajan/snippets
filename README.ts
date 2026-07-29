70cc26e24593afe52504dc99f75c3bc6ad0c85f8
timrajan <45014540+timrajan@users.noreply.github.com>
2026-07-29 15:06:20 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,4 @@
-ALTER DATABASE testdata WITH ALLOW_CONNECTIONS false;
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'testdata';
-CREATE DATABASE proddata WITH TEMPLATE testdata OWNER postgres;
-ALTER DATABASE testdata WITH ALLOW_CONNECTIONS true;
+SELECT column_name, data_type, is_nullable
+FROM information_schema.columns
+WHERE table_name = 'ClientData'
+ORDER BY ordinal_position;
