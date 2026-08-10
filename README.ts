51d9d1597a40776bbf8beb0c550d68161c7b0543
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-10 15:50:22 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,3 @@
 string.Join(" | ", _db.ChangeTracker.Entries<TestData>().Select(e => e.Entity.Id + ":" + e.Entity.TestId + ":" + e.State))
+
+string.Join(",", _db.Model.FindEntityType(typeof(TestData)).GetKeys().Single().Properties.Select(p => p.Name))
