b5e5e64c1fd2bd8f223248006cb1c09dacc7bf61
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-10 15:33:22 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,1 @@
-var et = _db.Model.FindEntityType(typeof(TestData))!;
-foreach (var k in et.GetKeys())
-    Console.WriteLine($"{(k.IsPrimaryKey() ? "PK" : "AK")}: {string.Join(",", k.Properties.Select(p => p.Name))}");
+When attaching existing entities, ensure that only one entity instance with a given key value is attached. Consider using 'DbContextOptionsBuilder.EnableSensitiveDataLogging' to see the conflicting key values.
