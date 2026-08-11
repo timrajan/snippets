f8fb18204bd3c0e0fd303d9ba1fcbfaacd79c94b
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-11 12:31:29 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,3 +1,2 @@
-string.Join(" | ", _db.ChangeTracker.Entries<TestData>().Select(e => e.Entity.Id + ":" + e.Entity.TestId + ":" + e.State))
-
-string.Join(",", _db.Model.FindEntityType(typeof(TestData)).GetKeys().Single().Properties.Select(p => p.Name))
+npx -y npm@latest approve-scripts --allow-scripts-pending
+npx -y npm@latest approve-scripts --all
