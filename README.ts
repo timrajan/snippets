57307804aa39b74fe626e82f9125cf773519ab5e
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-20 10:54:54 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,12 +1,4 @@
-const i = myDivContainer.querySelectorAll(".template-row").length;
-clone.querySelectorAll("input, select").forEach(el => {
-    el.value = "";
-    if (el.name) el.name = el.name.replace(/\[\d+\]/, `[${i}]`);
-    if (el.id)   el.id   = el.id.replace(/_\d+__/, `_${i}__`);
-});
-clone.querySelectorAll("[data-valmsg-for]").forEach(s =>
-    s.setAttribute("data-valmsg-for", s.getAttribute("data-valmsg-for").replace(/\[\d+\]/, `[${i}]`)));
-
-const form = $(myDivContainer.closest("form"));
-form.removeData("validator").removeData("unobtrusiveValidation");
-$.validator.unobtrusive.parse(form);
+[Required(ErrorMessage = "This field is required")]
+[RegularExpression(@"^\d+$", ErrorMessage = "Only numbers are allowed in this field")]
+[Display(Name = "Account Number")]
+public string AccountNumber { get; set; }
