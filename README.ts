805c24ba880308463f3db9293daf664965f49d4a
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-20 10:58:21 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,4 +1,5 @@
 [Required(ErrorMessage = "This field is required")]
 [RegularExpression(@"^\d+$", ErrorMessage = "Only numbers are allowed in this field")]
+  [RegularExpression(@"^\d{10}$", ErrorMessage = "Must be exactly 10 digits")]
 [Display(Name = "Account Number")]
 public string AccountNumber { get; set; }
