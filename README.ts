e0f08b1de5c8943cbb23382ac30d534ded310dd4
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-26 13:08:39 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,2 +1,4 @@
 app.MapGet("/routes", (IEnumerable<EndpointDataSource> sources) =>
-    string.Join("\n", sources.SelectMany(s => s.Endpoints).Select(e => e.DisplayName)));
+    string.Join("\n", sources.SelectMany(s => s.Endpoints)
+        .OfType<RouteEndpoint>()
+        .Select(e => e.RoutePattern.RawText)));
