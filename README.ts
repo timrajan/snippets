c64d4c52eb0b2f3bd362c48edd95b83398e2848f
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-26 10:03:55 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,15 +1,9 @@
-var result = allRecords
-    .Where(x => x.Status == "New")
-    .GroupBy(x => x.Email)
-    .OrderByDescending(g => g.Max(x => x.CreatedDate))
-    .Select(g => new UserSummaryViewModel
-    {
-        Email    = g.Key,
-        RowCount = g.Count(),
-        Ids      = string.Join(", ", g.Select(x => x.Id).OrderBy(i => i)),
-        IdsHtml  = string.Join("", g.Select(x => x.Id).OrderBy(i => i)
-                        .Select(id => $"<div class='py-1 border-bottom'>#{id}</div>"))
-                   + $"<button type='button' class='btn btn-sm btn-outline-secondary w-100 mt-2 copy-ids' data-ids='{string.Join(", ", g.Select(x => x.Id).OrderBy(i => i))}'>Copy IDs</button>"
-    })
-    .Take(6)
-    .ToList();
+@section Scripts {
+    <script>
+        $(function () {
+            $('[data-bs-toggle="popover"]').each(function () {
+                new bootstrap.Popover(this, { container: 'body' });
+            });
+        });
+    </script>
+}
