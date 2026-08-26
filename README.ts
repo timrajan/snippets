bdf2dd6a13861391766fa1268eda0a9eea6e1ac7
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-26 15:53:50 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,10 @@
-<tr><td colspan="99">DEBUG: @(Model == null ? "MODEL IS NULL" : Model.Count + " rows")</td></tr>
+<table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <th>Column One</th>
+            <th>Column Two</th>
+            <th>Column Three</th>
+        </tr>
+    </thead>
+    <tbody id="recordsBody"></tbody>
+</table>
