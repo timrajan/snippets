476e6ad83857476f3c3b29beb3f65477d02d91fa
timrajan <45014540+timrajan@users.noreply.github.com>
2026-08-27 10:14:50 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,21 +1,3 @@
-@section Scripts {
-<script>
-    function refreshPendingCount() {
-        if (document.hidden) return;
-        $.getJSON('/TestClient/PendingCount', function (d) {
-            $('#pendingBadge').text(d.count);
-            $('#pendingBtn').toggle(d.count > 0);
-        });
-    }
-
-    $(function () {
-        refreshPendingCount();
-        setInterval(refreshPendingCount, 30000);
-    });
-</script>
-}
-
-
-<a href="/TestClient/PendingRecords" class="btn btn-warning" id="pendingBtn" style="display:none;">
-    Pending Records <span class="badge bg-dark" id="pendingBadge">0</span>
-</a>
+    <div class="row pt-3 align-items-start">
+        <div class="col-6 col-md-3">
+            <div class="d-flex gap-2">
