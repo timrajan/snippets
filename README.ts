7fcd0d752be55192fc7010c6cc2180a1d4c0dcbf
timrajan <45014540+timrajan@users.noreply.github.com>
2026-09-09 13:15:46 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,1 +1,8 @@
-<td>@(row.RollNumber == 0 ? "NA" : row.RollNumber.ToString())</td>
+<td class="text-end">
+    <button type="button"
+            class="btn btn-sm btn-outline-secondary copy-row"
+            data-copy="@row.Name&#9;@row.RollNumber&#9;@row.Year&#9;@row.Gender"
+            title="Copy row">
+        <i class="bi bi-clipboard"></i>
+    </button>
+</td>
