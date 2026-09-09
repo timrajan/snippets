ac4b85e4f1766d15f53ffe9a48d52cf584e2a2e3
timrajan <45014540+timrajan@users.noreply.github.com>
2026-09-09 12:09:46 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,8 +1,7 @@
- if (!ModelState.IsValid)
-    {
-        vm.Years = AppLists.Students.Years;
-        vm.Genders = AppLists.Students.Genders;
-        vm.Rows = Array.Empty<TestData>();
-        vm.TotalCount = 0;
-        return View(vm);
-    }
+ <div class="col-12 col-md-6 col-lg-3">
+     <div class="py-1 form-floating mb-2">
+         <input asp-for="RollNumber" inputmode="numeric" maxlength="11" title ="RollNumber must be exactly 11 digits" class="form-control border b-shadow-none" placeholder=" "/>
+         <label asp-for="RollNumber">RollNumber</label>
+         <span asp-validation-for="RollNumber" class="text-danger small"></span>
+     </div>
+ </div>
