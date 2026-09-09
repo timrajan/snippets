4a0c7efa8c2787989bcb92bf927756100338a376
timrajan <45014540+timrajan@users.noreply.github.com>
2026-09-09 12:18:40 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,7 +1,1 @@
- <div class="col-12 col-md-6 col-lg-3">
-     <div class="py-1 form-floating mb-2">
-         <input asp-for="RollNumber" inputmode="numeric" maxlength="11" title ="RollNumber must be exactly 11 digits" class="form-control border b-shadow-none" placeholder=" "/>
-         <label asp-for="RollNumber">RollNumber</label>
-         <span asp-validation-for="RollNumber" class="text-danger small"></span>
-     </div>
- </div>
+<td>@(row.RollNumber == 0 ? "NA" : row.RollNumber.ToString())</td>
