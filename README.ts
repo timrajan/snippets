128494951de148339ef75c195f2441c5054f8e98
timrajan <45014540+timrajan@users.noreply.github.com>
2026-09-10 13:03:21 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,5 +1,8 @@
-const addInClone = clone.querySelector('.btn-success');
-if (!clone.querySelector('.remove-row')) {
-    addInClone.insertAdjacentHTML('afterend',
-        '<button type="button" class="btn btn-danger remove-row">−</button>');
-}
+<button type="button" class="btn btn-sm btn-outline-secondary edit-comment"
+            data-bs-toggle="modal" data-bs-target="#commentModal"
+            data-id="@row.Id"
+            data-name="@row.Name"
+            data-comment="@row.Comment"
+            title="Edit comment">
+        <i class="bi bi-pencil"></i>
+    </button>
