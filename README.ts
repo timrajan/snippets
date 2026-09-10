b3b912fd85967c655f86fb4b8ec2a99a8f3f221f
timrajan <45014540+timrajan@users.noreply.github.com>
2026-09-10 17:12:36 +1000

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,12 +1,13 @@
-@section Scripts {
-    <script>
-        $(function () {
-            $('.edit-comment').on('click', function () {
-                var btn = $(this);
-                $('#commentEmail').val(btn.data('email'));
-                $('#commentStudentName').text(btn.data('name'));
-                $('#commentText').val(btn.data('comment'));
-            });
-        });
-    </script>
+<div id="rowLimitMsg" class="alert alert-warning" role="alert" hidden>
+    You can add a maximum of 20 students.
+</div>
+
+    function refreshRowState() {
+    const count = container.querySelectorAll('.student-row').length;
+
+    container.querySelectorAll('.add-row').forEach(btn => {
+        btn.hidden = count >= MAX_ROWS;
+    });
+
+    document.getElementById('rowLimitMsg').hidden = count < MAX_ROWS;
 }
